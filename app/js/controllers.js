c6cdb3b070f0b3b86b7f1809432cf1895dbdd7a7
Hung BCP <hungbcp@asiantech.vn>
2016-03-07 17:12:00 +0700

    Let meet  service, also, request to get our data

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,25 +3,10 @@
 // App controllers
 var wfeApp = angular.module('wfeApp', []);
 
-wfeApp.controller('PeopleListCtrl', ['$scope', function ($scope) {
-  $scope.people = [
-    {
-      'empCode': 'AT0220',
-      'fullName': 'Đinh Quang Huy',
-    },
-    {
-      'empCode': 'AT0090',
-      'fullName': 'Đặng Công Đạo',
-    },
-    {
-      'empCode': 'AT0003',
-      'fullName': 'Trần Đức Lộc',
-    },
-    {
-      'empCode': 'AT0006',
-      'fullName': 'Lê Thị Thanh Phúc',
-    },
-  ];
+wfeApp.controller('PeopleListCtrl', ['$scope', '$http', function ($scope, $http) {
+  $http.get('data/people.json').success(function(data) {
+    $scope.people = data;
+  });
 
   $scope.order = 'empCode';
 }]);
