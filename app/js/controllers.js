bbf76615656ab87d7b33245f3b75f50557056c4f
Hung BCP <hungbcp@asiantech.vn>
2016-03-07 15:59:41 +0700

    Modelify our people list, declare app and its controller

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,3 +1,17 @@
 'use strict';
 
 // App controllers
+var wfeApp = angular.module('wfeApp', []);
+
+wfeApp.controller('PeopleListCtrl', ['$scope', function ($scope) {
+  $scope.people = [
+    {
+      'empCode': 'AT0220',
+      'fullName': 'Đinh Quang Huy',
+    },
+    {
+      'empCode': 'AT0090',
+      'fullName': 'Đặng Công Đạo',
+    },
+  ];
+}]);
